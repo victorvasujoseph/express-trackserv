0503c2e80989d907357d563acf48a697b1aedcbb
Victor Joseph <9651630+victorvasujoseph@users.noreply.github.com>
2019-12-22 21:07:09 -0800

    Auth Routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
-const express = require('express');
-const app = express();
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 const PORT = 9000;
 
-app.get('/',(req,res)=>{
-    res.send('Hi There !!');
+const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri =
+  "mongodb://victortrackserv:victor2019@ds257698.mlab.com:57698/react-tracking-db";
+if (!mongoUri) {
+  throw new Error(
+    `MongoURI was not supplied.  Make sure you watch the video on setting up Mongo DB!`
+  );
+}
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", err => {
+  console.error("Error connecting to mongo", err);
 });
 
-app.listen(PORT,()=>{
-    console.log("Listening on " + PORT);
-})
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on " + PORT);
+});
